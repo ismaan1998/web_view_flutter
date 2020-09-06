7b6e3e1fc0515b38be13cd874350c96ac4aa598b
Indrajeet Singh <indrajeetsinghmaan@gmail.com>
2020-09-06 14:34:53 +0530

    added slowgrowth academy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-import 'package:splashscreen/splashscreen.dart';
 import 'package:web_view_in_flutter/loading_widget.dart';
 
 
@@ -12,10 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'SlowGrowth Academy',
       home: SafeArea(
         child: WebviewScaffold(
-          url:"https://google.com", //example url
+          url:"https://slowgrowth.com", //example url
           withZoom: false,
           withJavascript: true,
           hidden: true,
